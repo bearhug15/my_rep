a64c954ef62ac95e1cb3a4f2605edf04984ad222
bearhug15 <iartem2000@gmail.com>
2019-10-20 15:20:47 +0700

    Update nucleotide.h

--- ОООП/lab1/nucleotide.h
+++ ОООП/lab1/nucleotide.h
@@ -5,16 +5,5 @@
 #include <iostream>
 
 enum class  nucleotide  { A=0, G=1, C=2, T=3 };
-/*std::ostream& operator<<(std::ostream& os, nucleotide nuc)
-{
-	switch (nuc)
-	{
-	case nucleotide::A: os << "A"; break;
-	case nucleotide::G: os << "G"; break;
-	case nucleotide::C: os << "C"; break;
-	case nucleotide::T: os << "T"; break;
-	default: os.setstate(std::ios_base::failbit);
-	}
-	return os;
-}*/
+std::ostream& operator<<(std::ostream& os, nucleotide nuc);
 #endif //LAB_DNK_NUCLEOTIDE_H
